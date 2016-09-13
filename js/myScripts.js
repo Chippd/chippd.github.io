70dae5cbb5799aea5eb357661d74007b66181e41
Chris Dermody <chrisdermody1@gmail.com>
2016-09-13 16:20:22 +0100

    some images added, styling is a little better

--- js/myScripts.js
+++ js/myScripts.js
@@ -9,7 +9,7 @@ $("#jobLocation").typeahead({ source:counties});
 
 
 
-$( "#homepageForm" ).submit(function( event ) {
+$( "#searchForm" ).submit(function( event ) {
   // console.log(this);
   event.preventDefault();
 
@@ -18,6 +18,58 @@ $( "#homepageForm" ).submit(function( event ) {
   console.log(keyword)
 
   //get location
-  var keyword = $(this).find("[name=location]").val();
-  console.log(keyword)
+  var location = $(this).find("[name=location]").val().toLowerCase();
+  // console.log(keyword)
+
+  //build url
+  var newUrl = "/search.html?q="+keyword+"&reg="+location
+
+  // console.log('redirect to:',newUrl)
+  window.location.assign(newUrl)
 });
+
+var loadSearchPage = function(){
+  console.log('loading search page');
+  var params = getParams();
+  getIrishJobs(params)
+}
+
+var getParams = function(){
+  //get query and location params from url
+  var keyword = location.search.split('&')[0]
+  keyword = keyword.slice(3, keyword.len);
+  console.log('keyword is:', keyword)
+
+  var region = location.search.split('&')[1]
+  region = region.slice(4, region.len);
+  console.log('region is:', region)
+  var params = {
+    keyword: keyword,
+    region: region
+  }
+
+  $('#searchForm ').find("[name=location]").val(params.region)
+  $('#searchForm ').find("[name=keyword]").val(params.keyword)
+
+  return params
+}
+
+var getIrishJobs = function(params) {
+  var source   = $("#job-template").html();
+  var template = Handlebars.compile(source);
+
+  $.get( "http://localhost:8080/scrapeirishjobs?q="+params.keyword+"&reg="+params.region, function( data ) {
+    // console.log('getIrishJobs ran:', data);
+    $(data).each(function(index){
+      // console.log(this)
+
+      this.siteLogo = "img/irishjobs.png"
+
+      var html = template(this);
+
+      $('#results').append(html)
+    })
+  });
+}
+
+
