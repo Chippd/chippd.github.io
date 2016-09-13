a2909fb8a1c81e7884d1725a113a4fb8513b3aba
Chris Dermody <chrisdermody1@gmail.com>
2016-09-13 15:00:56 +0100

    updated form to capture submit event, logging keyword and location to the console

--- js/myScripts.js
+++ js/myScripts.js
@@ -3,4 +3,21 @@
 var counties = ["Antrim", "Armagh", "Carlow", "Cavan", "Clare", "Cork", "Derry", "Donegal", "Down", "Dublin", "Fermanagh", "Galway", "Kerry", "Kildare", "Kilkenny", "Laois", "Leitrim", "Limerick", "Longford", "Louth", "Mayo", "Meath", "Monaghan", "Offaly", "Roscommon", "Sligo", "Tipperary", "Tyrone", "Waterford", "Westmeath", "Wexford", "Wicklow"]
 
 
-$("#jobLocation").typeahead({ source:counties})
+$("#jobLocation").typeahead({ source:counties});
+
+
+
+
+
+$( "#homepageForm" ).submit(function( event ) {
+  // console.log(this);
+  event.preventDefault();
+
+  //get job keyword
+  var keyword = $(this).find("[name=keyword]").val()
+  console.log(keyword)
+
+  //get location
+  var keyword = $(this).find("[name=location]").val();
+  console.log(keyword)
+});
