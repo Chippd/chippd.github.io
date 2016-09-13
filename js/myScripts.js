97019bd3ac74b3d6e73d9cce8819c42ad34a55d6
Chris Dermody <chrisdermody1@gmail.com>
2016-09-13 14:46:43 +0100

    added typeahead, is working for counties

---
+++ js/myScripts.js
@@ -0,0 +1,6 @@
+
+
+var counties = ["Antrim", "Armagh", "Carlow", "Cavan", "Clare", "Cork", "Derry", "Donegal", "Down", "Dublin", "Fermanagh", "Galway", "Kerry", "Kildare", "Kilkenny", "Laois", "Leitrim", "Limerick", "Longford", "Louth", "Mayo", "Meath", "Monaghan", "Offaly", "Roscommon", "Sligo", "Tipperary", "Tyrone", "Waterford", "Westmeath", "Wexford", "Wicklow"]
+
+
+$("#jobLocation").typeahead({ source:counties})
